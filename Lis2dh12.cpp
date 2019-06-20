1b0d09c9f3956ab5a32134e485c4217147cea283
Roxana Meixner <roxana.meixner@gmail.com>
2019-06-20 19:19:02 +0200

    [WIP] enhanced Lis2dh12 lib + quick fixes (not working)

--- Lis2dh12.cpp
+++ Lis2dh12.cpp
@@ -5,9 +5,74 @@
 #include <edebug.h>
 #include "Lis2dh12.h"
 
-Lis2dh12::Lis2dh12() {
 
+
+
+int32_t Lis2dh12::platform_read(uint8_t regAddr, uint8_t *buff, uint16_t buffSize) {
+    // Most significant bit represents read from register, bit after it
+    // represents address increment if multiple read, which is enabled.
+    const uint8_t spiSetup = 0xC0;
+
+    uint8_t retVal = 0;
+    *cs = 0;
+
+    spi->write(spiSetup | regAddr);
+
+    while(buffSize--)
+    {
+        *buff = spi->write(0x00);
+        buff++;
+    }
+
+    *cs = 1;
+    return retVal;
 }
+
+int32_t Lis2dh12::platform_write(uint8_t regAddr, uint8_t *buff, uint16_t buffSize) {
+    // Most significant bit represents write from register, bit after it
+    // represents address increment if multiple write, which is enabled.
+    const uint8_t spiSetup = 0x40;
+
+    uint8_t retVal = 0;
+
+    *cs = 0;
+
+    spi->write(spiSetup | regAddr);
+
+    while(buffSize--)
+    {
+        spi->write(*buff);
+        buff++;
+    }
+
+    *cs = 1;
+    return retVal;
+}
+
+int32_t write(void *handle, uint8_t regAddr, uint8_t* buff, uint16_t buffSize){
+    Lis2dh12 *sensor = (Lis2dh12*)handle;
+    sensor->platform_write(regAddr, buff, buffSize);
+    return 0;
+}
+
+int32_t read(void *handle, uint8_t regAddr, uint8_t* buff, uint16_t buffSize){
+    Lis2dh12 *sensor = (Lis2dh12*)handle;
+    sensor->platform_read(regAddr, buff, buffSize);
+    return 0;
+}
+
+Lis2dh12::Lis2dh12(SPI *_spi, DigitalOut *_cs):
+        spi(_spi),
+        cs(_cs)
+{
+    //dev_ctx = new lis2dh12_ctx_t;
+
+    dev_ctx.write_reg = write;
+    dev_ctx.read_reg = read;
+    dev_ctx.handle = this;
+}
+
+
 
 Lis2dh12::~Lis2dh12() {
 }
@@ -52,7 +117,7 @@ int32_t Lis2dh12::init() {
     return error;
 }
 
-int32_t Lis2dh12::read(float *x_axis, float *y_axis, float *z_axis){
+int32_t Lis2dh12::readAxis(float *x_axis, float *y_axis, float *z_axis){
     uint8_t ready;
     int32_t error;
 
--- Lis2dh12.h
+++ Lis2dh12.h
@@ -10,13 +10,13 @@
 
 class Lis2dh12 {
 public:
-    Lis2dh12();
+    Lis2dh12(SPI *_spi, DigitalOut *_cs);
 
     virtual ~Lis2dh12();
 
-    void init();
+    int32_t init();
 
-    int32_t read(float *x_axis, float *y_axis, float *z_axis);
+    int32_t readAxis(float *x_axis, float *y_axis, float *z_axis);
 
 private:
     axis3bit16_t data_raw_acceleration;
@@ -24,7 +24,13 @@ private:
     uint8_t whoamI;
     uint8_t tx_buffer[1000];
 
+    SPI *spi;
+    DigitalOut *cs;
     lis2dh12_ctx_t dev_ctx;
+
+public:
+    int32_t platform_read(uint8_t regAddr, uint8_t *buff, uint16_t buffSize);
+    int32_t platform_write(uint8_t regAddr, uint8_t *buff, uint16_t buffSize);
 
 };
 
