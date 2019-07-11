4b06bd9c0666b84e2226067ee7ab0e6098bab194
Roxana Meixner <roxana.meixner@gmail.com>
2019-07-11 09:05:58 +0200

    doxygen author, date, license

--- Lis2dh12.h
+++ Lis2dh12.h
@@ -1,6 +1,26 @@
-//
-// Created by larox on 20.06.19.
-//
+/*!
+ * @file
+ * @brief Lis2dh12 Accelerometer
+ *
+ * @author Roxana Meixner
+ * @date   2019-06-20
+ *
+ * @copyright &copy; 2017 ubirch GmbH (https://ubirch.com)
+ *
+ * ```
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ```
+ */
 
 #ifndef UBIRCH_ENERTHING_FIRMWARE_LIS2DH12_H
 #define UBIRCH_ENERTHING_FIRMWARE_LIS2DH12_H
